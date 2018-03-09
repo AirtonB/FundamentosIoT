37861bc71aa05ddc4e0276331f663002d7a8977e
AirtonB <leandrobezerra94@hotmail.com>
2018-03-09 01:17:21 -0300

    Update 2_DecimalToBinario.c

--- Atividade-1/2_DecimalToBinario.c
+++ Atividade-1/2_DecimalToBinario.c
@@ -4,23 +4,24 @@
  Created on 2 de Março de 2018, 20:01
  */
 #include <stdio.h>
- 
+#include <stdlib.h>
+
 int main()
 {
-  int num,bit, x;
- 
-  printf("Digite um número inteiro: \n");
-  scanf("%d", &num);
- 
-  printf("%d (Decimal) em binário é: \n", num);
-  for (bit = 31; bit >= 0; bit--){
-      x = num >> bit;
-      if (x & 1){
-        printf("1");
-      }else{
-          printf("0");
-      }
+    int num, bit, x;
+    printf("\n### Conversor de Números Inteiros para Binário ### \n");
+    printf("Digite um número inteiro: \n");
+    scanf("%d", &num);
+
+    printf("%d (Decimal) em binário é: \n", num);
+    for (bit = 31; bit >= 0; bit--){
+        x = num >> bit;
+        if (x & 1){
+            printf("1");
+        }else{
+            printf("0");
+        }
     }
     printf("\n\n");
-  return 0;
+    return 0;
 }
